afa06367fc42a732f772dfa96841120c74468c71
Ongchenwen <ongchen9788@gmail.com>
2020-09-14 15:27:34 -0400

    Changed LAYOUT

--- src/ComputerStore.java
+++ src/ComputerStore.java
@@ -1,75 +1,96 @@
+import java.awt.print.Book;
 import java.util.Scanner;
 
 public class ComputerStore {
 
     final static String password = "password";
-    Scanner input;
+    private static final int SIZE = 10;
+    Computer[] computerList = new Computer[SIZE];
+    int counter = 0;
 
     public static void displayMenu(){
-        System.out.println("**** Welcome ****");
         System.out.println("What do you want to do?");
-        System.out.println("1. Enter new computer (Password Required");
-        System.out.println("2. Change information of a computer (Password Required)");
-        System.out.println("3. Display all computers by a specific brand");
-        System.out.println("4. Display all computers under a certain a price.");
-        System.out.println("5. Quit");
+        System.out.println("    1. Enter new computer (Password Required)");
+        System.out.println("    2. Change information of a computer (Password Required)");
+        System.out.println("    3. Display all computers by a specific brand");
+        System.out.println("    4. Display all computers under a certain a price.");
+        System.out.println("    5. Quit");
 
 
     }
 
-    public static void main(String[] args) {
+    public ComputerStore(){
+        Scanner scan = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
-        int enterTimes = 3;
+        System.out.println("**** Welcome ****");
+        displayMenu();
 
-        System.out.println("Please enter the maximum capacity of computers : ");
-        int capacityOfComputer = input.nextInt();
+        while (true){
+            int choice = scan.nextInt();
 
-        // Array of Computers
-        Computer[] inventory = new Computer[capacityOfComputer];
+            if(choice == 5){
+               System.out.println("Thanks for visiting!!");
+               break;
+            }
 
-
-        while (true) {
-            switch (input.nextInt()) {
-                case 1: {
-                    int remindingSpace = capacityOfComputer;
-                    System.out.println("Please enter your password!");
-                    if (input.nextLine() == password) {
-                        System.out.println("Pleas enter number of computers you want:");
-                        int inputNum = input.nextInt();
-                        //check if it has enough space for input
-                        if(inputNum > remindingSpace){
-                            System.out.println("Error! Reminding space is : " + remindingSpace);
-                        }else {
-                                for(int i =0; i< remindingSpace; i++){
-                                    System.out.println("Please enter computer brand: ");
-                                    String brand = input.nextLine();
-                                    System.out.println("Please enter computer model: ");
-                                    String model = input.nextLine();
-                                    System.out.println("Please enter computer Serial Number: ");
-                                    long serialNumber = input.nextLong();
-                                    System.out.println("Please enter price: ");
-                                    double price = input.nextDouble();
-                                    inventory[i] = new Computer(brand,model,serialNumber,price);
-                                    remindingSpace -=1;
-                                }
-
-                            }
-                        } else {
-                        // 3 times done, display menu
-                        enterTimes -=1;
-                        if(enterTimes == 0){
-                            displayMenu();
-                        }
-
+            switch (choice){
+                case 1:{
+                    System.out.println("Please enter password");
+                    String keyPassword = scan.next();
+                    if(keyPassword.equals(password)){
+                        addComputer();
                     }
                     }
-
-
-                }
+                case 2:modifyComputer();
+                case 3:DisplayCompBySpecifiedBrand();
+                case 4:DisplayCompByPrice();
 
             }
         }
+
+    }
+
+    public void addComputer() {
+    if(counter < SIZE){
+        System.out.println("Reminding Space : " + counter);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("----------------------");
+
+            System.out.println("Please enter computer brand: ");
+            String brand = input.nextLine();
+            System.out.println("Please enter computer model: ");
+            String model = input.nextLine();
+            System.out.println("Please enter computer Serial Number: ");
+            long serialNumber = input.nextLong();
+            System.out.println("Please enter price: ");
+            double price = input.nextDouble();
+            Computer computer = new Computer(brand,model,serialNumber,price);
+            computerList[counter] = computer;
+            counter++;
+
+            System.out.println("New computer Added");
+        }
+    }
+
+
+
+    public void modifyComputer(){
+
+    }
+
+    public void DisplayCompBySpecifiedBrand() {
+
+    }
+
+    public void DisplayCompByPrice(){
+
+    }
+
+
+    public static void main(String[] args) {
+        new ComputerStore();
+    }
 }
 
 
